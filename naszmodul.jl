ae8d4af499a8d057b483464fe268d11eee484d2f
Nattq <nataliajelito3@gmail.com>
2021-05-20 13:31:14 +0200

    dadana funkcja loess i denoising, plus opisy

--- naszmodul.jl
+++ naszmodul.jl
@@ -46,7 +46,7 @@ function idft(signal)
     [  (1/N)*sum( signal[n + 1] * zeta_powers[(n * f) % N] for n in 0:(N-1)   ) for f in 0:(N-1) ]
 end
 
-"""Funkcja wycinająca mniejsze wielkości"""
+"""Funkcja wycinająca mniejsze wielkości(tzn. cichsze, na podstawie PSD)"""
 function denoising_high(sound, fs, N,cut_point)
     tstep=1/fs
     time=LinRange(0, (N-1)/fs, N)
@@ -63,7 +63,7 @@ function denoising_high(sound, fs, N,cut_point)
     return time, PSD_clean, fhat_clean, freq
 end
 
-"""Funkcja wycinająca większe wielkości"""
+"""Funkcja wycinająca większe wielkości(tzn. głośniejsze, na podstawie PSD)"""
 function denoising_low(sound, fs, N,cut_point)
     tstep=1/fs
     time=LinRange(0, (N-1)/fs, N) 
@@ -94,14 +94,14 @@ function remove_frequency(sound, freq_to_remove, dt = 0.001, start = 0, stop = 2
     real(ifft(fhat_clean)), freq, PSD_clean, fhat_clean
 end
 
-"""Funkcja do przyspieszania(?)"""
+"""Funkcja do przyspieszania/zwalniania tempa odtwarzania"""
 function change_speed(sound, fs, speed, file_name)
     wavwrite(sound, file_name, Fs = fs * speed)
     sound_speeded, fs_speeded = wavread(file_name)
     return sound_speeded, fs_speeded
 end
 
-"""Funkcja do zmiany głośniści(?)"""
+"""Funkcja do zmiany głośniści (ściszanie i zgłaśnianie)"""
 function change_volume(sound, volume)
     sound_fft = fft(sound)
     new_sound_fft = volume .* sound_fft
@@ -124,6 +124,34 @@ function cutting_time(sound,fs,start=0,stop=((length(sound[:,1])-1)/fs))
     time_new=LinRange(0,(N_new-1)/fs,N_new)
     
     return time_new, new_sound
+end
+
+"""Oszacowanie wartości w zadanym punkcie za pomocą regresji lokalnej"""
+function loess(index, X,Y,m)
+    if index <= m
+        xs = X[1:(index+m)]
+        ys = Y[1:(index+m)]
+    elseif index + m <= length(X)
+        xs = X[(index-m):(index+m)]
+        ys = Y[(index-m):(index+m)]
+    else
+        xs = X[(index-m):end]
+        ys = Y[(index-m):end]    
+    end
+    av_x = sum(xs)/length(xs) 
+    av_y = sum(ys)/length(ys)
+    
+    a = sum((xs .- av_x) .* (ys .- av_y) )/ sum( (xs .- av_x).^2)
+    b = av_y - a * av_x
+    return a*X[index]+b
+end
+
+"""Funkcja, która za pomocą loess znajduje przybliżenie funkcji.
+     Uzywamy do odszumiania dzwieków"""
+function denoising(Y,m= 10)
+    X = 1:length(Y)
+    y = [loess(index, X,Y,m) for index in 1:length(X)]
+    return y
 end
 
 end
